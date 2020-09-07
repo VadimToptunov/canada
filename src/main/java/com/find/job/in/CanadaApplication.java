826c6496b8e09659abc17ae7a47c4a17b0f3d77b
“Vadim <vadim_toptunov@epam.com>
2020-09-07 14:24:11 +0300

    Create project, add html and css files

---
+++ src/main/java/com/find/job/in/CanadaApplication.java
@@ -0,0 +1,13 @@
+package com.find.job.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/find/job/in/CanadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.find.job.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
